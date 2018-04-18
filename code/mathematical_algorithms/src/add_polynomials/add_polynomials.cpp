7439b1fddf1e54799d5c414d0ce46c7dac77569b
Christian Bender <christianbender89@web.de>
2018-04-18 17:23:46 +0200

    change something

--- code/mathematical_algorithms/src/add_polynomials/add_polynomials.cpp
+++ code/mathematical_algorithms/src/add_polynomials/add_polynomials.cpp
@@ -21,94 +21,61 @@ term::term(int c, int p) {
 }
 
 class Polynomial {
-  term* head;
+  vector<term> poly;
 
  public:
-  Polynomial();
+  Polynomial(){}
   void insert_term(int, int);
   void print();
   friend Polynomial operator+(Polynomial, Polynomial);
 };
 
-Polynomial::Polynomial() { head = NULL; }
 
 void Polynomial::insert_term(int c, int p) {
-  if (head == NULL) {
-    head = new term(c, p);
-    return;
-  }
-  if (p > head->pow) {
-    term* t = new term(c, p);
-    t->next = head;
-    head = t;
-    return;
-  }
-  term* cur = head;
-  while (cur != NULL) {
-    if (cur->pow == p) {
-      cur->coeff += c;
-      return;
+  bool found = false;
+  for(int i = 0; i < poly.size(); i++) {
+    if (poly[i].pow == p) {
+      poly[i].coeff = c;
+      found = true;
+      break;
     }
-    if ((cur->next == NULL) || (cur->next->pow < p)) {
-      term* t = new term(c, p);
-      t->next = cur->next;
-      cur->next = t;
-      return;
-    }
-    cur = cur->next;
+  }
+  if (!found) {
+    poly.push_back(term(c,p));
   }
 }
 
 void Polynomial::print() {
-  term* t = head;
-  while (t != NULL) {
-    cout << t->coeff;
-    if (t->pow) cout << "x^" << t->pow;
-    if (t->next != NULL) cout << "+";
-    t = t->next;
+  for(int i = 0; i < poly.size(); i++) {
+    if (i == 0) {
+      cout << poly[i].coeff << "x^" << poly[i].pow;
+    } else if (i < poly.size() -1) {
+      cout << poly[i].coeff << "x^" << poly[i].pow << " + ";
+    } else {
+      cout << poly[i].coeff << "x^" << poly[i].pow;
+    }
   }
-  cout << endl;
 }
 
 Polynomial operator+(Polynomial p1, Polynomial p2) {
-  Polynomial p;
-  term *t1 = p1.head, *t2 = p2.head;
-  while ((t1 != NULL) && (t2 != NULL)) {
-    if (t1->pow > t2->pow) {
-      p.insert_term(t1->coeff, t1->pow);
-      t1 = t1->next;
-    } else if (t1->pow < t2->pow) {
-      p.insert_term(t2->coeff, t2->pow);
-      t2 = t2->next;
-    } else {
-      p.insert_term(t1->coeff + t2->coeff, t1->pow);
-      t1 = t1->next;
-      t2 = t2->next;
-    }
-  }
-  while (t1 != NULL) {
-    p.insert_term(t1->coeff, t1->pow);
-    t1 = t1->next;
-  }
-  while (t2 != NULL) {
-    p.insert_term(t2->coeff, t2->pow);
-    t2 = t2->next;
-  }
-  return p;
+  vector<term> poly1 = p1.poly;
+  vector<term> poly2 = p2.poly;
+
 }
 
 int main() {
   Polynomial p1, p2;
-  p1.insert_term(7, 4);
   p1.insert_term(4, 5);
-  p1.insert_term(10, 0);
+  p1.insert_term(7, 4);
   p1.insert_term(9, 2);
+  p1.insert_term(10, 0);
   cout << "First polynomial:";
   p1.print();
-  p2.insert_term(5, 0);
   p2.insert_term(6, 5);
-  p2.insert_term(7, 0);
   p2.insert_term(3, 2);
+  p2.insert_term(5, 0);
+  p2.insert_term(7, 0);
+  
   cout << "Second polynomial:";
   p2.print();
   Polynomial p3 = p1 + p2;
