761e37e9b424da46032d6d3237f46836d9684f42
Christian Bender <christianbender89@web.de>
2018-04-19 18:04:39 +0200

    changed the project

--- code/bit_manipulation/src/bit_division/bitDivision.c
+++ code/bit_manipulation/src/bit_division/bitDivision.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
-// Part of Cosmos by OpenGenus Foundation
-unsigned bitDivision(unsigned numerator, unsigned denominator) {
+#include <assert.h>  /* for the tests */
+
+// Prototypes
+void test(void);
+unsigned bit_division(unsigned, unsigned);
+
+int main(void) {
+    test();
+    return 0;
+}
+
+void test(void){
+    assert(bit_division(23,5) == 4);
+    assert(bit_division(25,5) == 5);
+    assert(bit_division(5,5) == 1);
+    assert(bit_division(3,5) == 0);
+    printf("tests successful\n");
+}
+
+
+unsigned bit_division(unsigned numerator, unsigned denominator) {
 
     unsigned current = 1;
-    unsigned answer=0;
+    unsigned answer = 0;
 
     if ( denominator > numerator)
         return 0;
@@ -19,7 +38,7 @@ unsigned bitDivision(unsigned numerator, unsigned denominator) {
     denominator >>= 1;
     current >>= 1;
 
-    while (current!=0) {
+    while (current) {
         if ( numerator >= denominator) {
             numerator -= denominator;
             answer |= current;
@@ -27,15 +46,6 @@ unsigned bitDivision(unsigned numerator, unsigned denominator) {
         current >>= 1;
         denominator >>= 1;
     }
+    
     return answer;
-}
-
-void test(){
-    int numerator = 23, denominator = 5;
-    printf("%d\n", bitDivision(numerator,denominator));
-}
-
-int main() {
-    test();
-    return 0;
 }
